c52044de2abd067104ab2dee1ddfaaa5ec817c61
Nick Chapman <nic@amadido.co.uk>
2021-03-07 08:25:37 +0000

    use haskeline fo console

--- src/Top.hs
+++ src/Top.hs
@@ -1,12 +1,15 @@
-module Top where
+module Top (main) where
 
+import Control.Monad.Trans.Class (lift)
 import FileSystem (fs0)
 import Interaction (Interaction(..))
 import Misc (EOF(..))
 import Os (Prog)
-import System.IO (hFlush,stdout)
 import qualified Bash (console)
 import qualified Os (sim)
+import qualified System.Console.ANSI as AN
+import qualified System.Console.Haskeline as HL
+import qualified System.Console.Haskeline.History as HL
 import qualified Tests (run)
 
 main :: IO ()
@@ -18,24 +21,57 @@ main = do
 prog :: Prog ()
 prog = Bash.console
 
--- TODO: use haskeline so get a nice console, with history!
 runInteraction :: Interaction -> IO ()
-runInteraction = loop where
-  loop :: Interaction -> IO ()
-  loop = \case
+runInteraction i0 = do
+  HL.runInputT haskelineSettings $ do
+    initialiseHistory
+    loop (1::Int) i0
+  where
+    loop n = \case
+      I_Read f -> do
+        HL.getInputLine (col AN.Green $ show n <> "> ") >>= \case
+          Nothing -> loop (n+1) (f (Left EOF))
+          Just line -> do
+            updateHistory line
+            loop (n+1) (f (Right line))
 
-    I_Read f -> do
-      putStr $ "> "
-      hFlush stdout
-      line <- getLine
-      let res = if line == "" then Left EOF else Right line
-      loop (f res)
+      I_Write line i -> do
+        lift $ putStrLn line
+        loop n i
 
-    I_Write line i -> do
-      putStrLn line
-      loop i
-    I_Trace s i -> do
-      putStrLn $ "(trace) " ++ s
-      loop i
-    I_Halt -> do
-      putStrLn "*halt*"
+      I_Trace mes i -> do
+        lift $ putStrLn $ "(trace) " ++ mes
+        loop n i
+
+      I_Halt -> do
+        lift $ putStrLn "*halt*"
+
+
+col :: AN.Color -> String -> String
+col c s =
+  AN.setSGRCode [AN.SetColor AN.Foreground AN.Vivid c] <> s <>
+  AN.setSGRCode [AN.SetColor AN.Foreground AN.Vivid AN.White]
+
+
+haskelineSettings :: HL.Settings IO
+haskelineSettings = HL.defaultSettings {HL.autoAddHistory = False}
+
+
+-- keep history in opposite order from HL standard (newest at end of file)
+
+histFile :: String
+histFile = ".history"
+
+initialiseHistory :: HL.InputT IO ()
+initialiseHistory = do
+  history <- lift (HL.readHistory histFile)
+  HL.putHistory (revHistory history)
+
+updateHistory :: String -> HL.InputT IO ()
+updateHistory line = do
+  HL.modifyHistory (HL.addHistory line)
+  history <- HL.getHistory
+  lift (HL.writeHistory histFile (revHistory history))
+
+revHistory :: HL.History -> HL.History
+revHistory = foldl (flip HL.addHistory) HL.emptyHistory . HL.historyLines
