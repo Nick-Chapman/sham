7aba630d6a0176b04d0e2d548db5f1b92616cd44
Nick Chapman <nic@amadido.co.uk>
2021-03-22 08:33:48 +0000

    kill pre-rewrite sham script code

--- src/Script.hs
+++ src/Script.hs
@@ -4,7 +4,6 @@ module Script (
   Step(..), Redirect(..), RedirectSource(..),
   Invocation(..),
   Pred(..),Word(..),Var(..),
-  --run,
   runScript, Env(..)
   ) where
 
@@ -17,14 +16,6 @@ import Prog (FD,SysCall(..),BadFileDescriptor(..),Pid(..),Prog,Command(..),OpenM
 import qualified Data.Map.Strict as Map
 import qualified Path (create)
 import qualified Prog (Prog(..))
-
-{-_run :: ([String] -> Script) -> Script -> Prog ()
-_run shamParser script = do
-  Command(com,args) <- Prog.Argv
-  pid <- Prog.MyPid
-  let bindings = Map.empty
-  let env = Env { pid, com, args, bindings, shamParser }
-  runScript env script-}
 
 data Env = Env
   { com :: String
@@ -80,336 +71,14 @@ data Redirect = Redirect OpenMode FD RedirectSource
 data RedirectSource = FromPath Word | FromFD FD
   deriving Show
 
-
-
 runScript :: Env -> Script -> Prog ()
-runScript env script =
-  if old then runScriptM (X []) env script else do
-    let s = conv script
-    Prog.Trace (show s)
-    runScrip env s
-  where old = False
-
-runScriptM :: Mode -> Env -> Script -> Prog ()
-runScriptM _mode env0 script0 = loop env0 script0 (\_ -> pure ()) where
-
-  loop :: Env -> Script -> (Env -> Prog ()) -> Prog ()
-  loop env = \case
-    ShamError s -> \_k -> do write stderr s
-    Seq s1 s2 -> \k -> loop env s1 (\env -> loop env s2 k)
-
-    If pred s1 s2 -> \k -> do
-      b <- evalPred env pred
-      loop env (if b then s1 else s2) k
-
-    ReadIntoVar x -> \k -> do
-      line <- builtinRead
-      let Env{bindings} = env
-      k env { bindings = Map.insert x line bindings }
-
-    Null -> \k -> do
-      k env
-
-    Invoke1 step wm -> \k -> do
-      _runStep env wm step
-      -- _doStep (addWait mode wm) env step
-      k env
-
-    Pipeline steps wm -> \k -> do
-      runPipeline env wm steps
-      k env
-
-builtinRead :: Prog String
-builtinRead =
-  read NoPrompt stdin >>= \case
-    Left EOF -> exit
-    Right line -> return line
-
-
-runPipeline :: Env -> WaitMode -> [Step] -> Prog ()
-runPipeline env wm = \case
-    [] -> error "runPipeline[]"
-    step1:steps -> loop Nothing [] step1 steps
-  where
-    loop :: Maybe FD -> [Pid] -> Step -> [Step] -> Prog ()
-    loop incoming pids step1 = \case
-      [] -> do
-        Prog.Fork >>= \case
-
-          Nothing -> do
-            case incoming of
-              Nothing -> pure ()
-              Just incoming -> do dup2 stdin incoming; Prog.Call Close incoming
-            execStep env step1
-
-          Just childPid -> do
-            case incoming of
-              Nothing -> pure ()
-              Just incoming -> Prog.Call Close incoming
-            case wm of
-              Wait -> mapM_ Prog.Wait (childPid:pids)
-              NoWait -> pure ()
-
-      step2:steps -> do
-        PipeEnds{w=pipeW,r=pipeR} <- Prog.Call SysPipe ()
-        Prog.Fork >>= \case
-
-          Nothing -> do
-            case incoming of
-              Nothing -> pure ()
-              Just incoming -> do dup2 stdin incoming; Prog.Call Close incoming
-            Prog.Call Close pipeR
-            dup2 stdout pipeW; Prog.Call Close pipeW
-            execStep env step1
-
-          Just childPid -> do
-            case incoming of
-              Nothing -> pure ()
-              Just incoming -> Prog.Call Close incoming
-            Prog.Call Close pipeW
-            loop (Just pipeR) (childPid:pids) step2 steps
-
-
-decode :: String -> Act
-decode = \case
-  "exit" -> Exit
-  "echo" -> Echo
-  "exec" -> Exec
-  "." -> SourceSham
-  name -> RunExternal name
-
-data Act
-  = Exit
-  | Echo -- TODO: rename BuiltinEcho
-  | SourceSham
-  | RunExternal String
-  | Exec
-
-
--- "exec" means we run this thing, and dont continue afterwards!
--- "run" means we run this thing, and continue
-
-execStep :: Env -> Step -> Prog ()
-execStep env  = \case
-  XSubShell script rs ->
-    --error "broken" $
-    --maybeSubshell env (runScript env script) (rs,Wait)
-    undefined script rs
-  Run invocation rs -> do
-    doInvocation (X rs) env invocation
-
-_runStep :: Env -> WaitMode -> Step -> Prog ()
-_runStep env wm = \case
-  XSubShell script rs -> do
-    let mode = undefined
-    maybeSubshell env (runScriptM mode env script) (rs,wm)
-  Run invocation rs -> do
-    doInvocation (R rs wm) env invocation
-
-_doStep :: Mode -> Env -> Step -> Prog ()
-_doStep mode env = \case
-  XSubShell script rs -> do
-    --maybeSubshell env (runScriptM mode env script) (rs,wm)
-    forkWait (runScriptM (addRs mode rs) env script)
-  Run invocation rs -> do
-    --doInvocation (R rs wm) env invocation
-    -- TODO: add rs into mode!
-    doInvocation (addRs mode rs) env invocation
-
-addRs :: Mode -> [Redirect] -> Mode
-addRs mode rs2 = case mode of
-  X rs -> X (rs ++ rs2)
-  R rs wm -> R (rs ++ rs2) wm
-
-_addWait :: Mode -> WaitMode -> Mode
-_addWait mode wm = case mode of
-  X rs -> R rs wm
-  R rs _ -> R rs wm
-
-doInvocation :: Mode -> Env -> Invocation -> Prog ()
-doInvocation mode env = \case
-  Invocation w ws -> do
-    com <- evalWord env w
-    args <- mapM (evalWord env) ws
-    doStage mode env args (decode com)
-
-data Mode = X [Redirect] | R [Redirect] WaitMode
-
-doStage :: Mode -> Env -> [String] -> Act -> Prog ()
-doStage mode env args = \case
-  Exit ->
-    case args of
-      [] -> exit
-      _ -> write stderr "exit takes no args"
-  Echo ->
-    case mode of
-      X rs -> redirecting env rs (execEcho args)
-      R rs wm  -> maybeSubshell env (runEcho args) (rs,wm)
-
-  SourceSham ->
-    case args of
-      com:args -> do
-        script <- loadShamScript env com
-        runScript env { args } script -- TODO: mode?
-      _ ->
-        write stderr "source takes at least one argument"
-
-  RunExternal name -> do
-    case mode of
-      X rs ->
-        redirecting env rs $ execCommand (Command (name,args))
-      R rs wm ->
-        subshell env (rs,wm) (execCommand (Command (name,args)))
-
-  Exec -> do
-    (case mode of X rs -> redirecting env rs; R rs _ -> redirecting env rs)
-      $ case args of
-          [] -> pure ()
-          name:args ->
-            execCommand (Command (name,args))
-
-
-maybeSubshell :: Env -> Prog () -> ([Redirect],WaitMode) -> Prog ()
-maybeSubshell env prog = \case
-  -- we must skip the subshell if there are no redircts and we wait
-  ([],Wait) -> prog
-  (rs,wm) -> do -- otherwise we must fork
-    subshell env (rs,wm) prog
-
-subshell :: Env -> ([Redirect],WaitMode) -> Prog () -> Prog ()
-subshell env (rs,wm) prog = do
-  forkMode wm $ do
-    redirecting env rs (do prog; exit)
-
-
-forkMode :: WaitMode -> Prog () -> Prog ()
-forkMode = \case
-  Wait -> forkWait
-  NoWait -> forkNoWait
-
-redirecting :: Env -> [Redirect] -> Prog () -> Prog ()
-redirecting env rs prog = do
-  mapM_ (execRedirect env) rs
-  prog
-
-
-runEcho :: [String] -> Prog ()
-runEcho args =
-  write stdout (unwords args) --builtin echo
-
-execEcho :: [String] -> Prog ()
-execEcho args = do
-  runEcho args
-  exit
-
-
-
-
-loadShamScript :: Env -> String -> Prog Script
-loadShamScript env path = do
-  lines <- do
-    withOpen (Path.create path) OpenForReading $ \fd -> do
-      readAll fd
-  pure $ shamParser env lines
-
-
-evalPred :: Env -> Pred -> Prog Bool
-evalPred env = \case
-  Eq w1 w2 -> do
-    x1 <- evalWord env w1
-    x2 <- evalWord env w2
-    pure (x1 == x2)
-  NotEq w1 w2 -> do
-    x1 <- evalWord env w1
-    x2 <- evalWord env w2
-    pure (x1 /= x2)
-
-evalWord :: Env -> Word -> Prog String
-evalWord Env{pid,com,args,bindings} = \case
-  Word s -> pure s
-  DollarDollar -> let (Pid n) = pid in pure $ show n
-  DollarHash -> pure $ show (length args)
-  DollarN n ->
-    if n > length args
-    then do
-      write stderr ("$" ++ show n ++ " unbound")
-      pure ""
-    else pure $ (com:args)!!n
-  DollarName x ->
-    case Map.lookup x bindings of
-      Nothing -> do
-        write stderr ("$" ++ show x ++ " unbound")
-        pure ""
-      Just v ->
-        pure v
-
-execRedirect :: Env -> Redirect -> Prog ()
-execRedirect env = \case
-  Redirect om dest (FromPath path) -> do
-    path <- evalWord env path
-    withOpen (Path.create path) om $ \src -> do
-      dup2 dest src
-  Redirect _om dest (FromFD src) -> do -- do we care what the open-mode is?
-    dup2 dest src
-
-dup2 :: FD -> FD -> Prog ()
-dup2 d s = do
-  Prog.Call Dup2 (d,s) >>= \case
-    Left BadFileDescriptor -> do
-      write stderr $ "bad file descriptor: " ++ show s
-      exit
-    Right () -> pure ()
-
-
-
-----------------------------------------------------------------------
--- new
-
-
-pipeline :: [Prog()] -> Prog ()
-pipeline = \case
-    [] -> error "runPipeline[]"
-    prog1:progs -> loop Nothing [] prog1 progs
-  where
-    loop :: Maybe FD -> [Pid] -> Prog () -> [Prog ()] -> Prog ()
-    loop incoming pids prog1 = \case
-      [] -> do
-        Prog.Fork >>= \case
-
-          Nothing -> do
-            case incoming of
-              Nothing -> pure ()
-              Just incoming -> do dup2 stdin incoming; Prog.Call Close incoming
-            prog1
-
-          Just childPid -> do
-            case incoming of
-              Nothing -> pure ()
-              Just incoming -> Prog.Call Close incoming
-            mapM_ Prog.Wait (childPid:pids)
-
-      prog2:progs -> do
-        PipeEnds{w=pipeW,r=pipeR} <- Prog.Call SysPipe ()
-        Prog.Fork >>= \case
-
-          Nothing -> do
-            case incoming of
-              Nothing -> pure ()
-              Just incoming -> do dup2 stdin incoming; Prog.Call Close incoming
-            Prog.Call Close pipeR
-            dup2 stdout pipeW; Prog.Call Close pipeW
-            prog1
-
-          Just childPid -> do
-            case incoming of
-              Nothing -> pure ()
-              Just incoming -> Prog.Call Close incoming
-            Prog.Call Close pipeW
-            loop (Just pipeR) (childPid:pids) prog2 progs
+runScript env script = do
+  let s = conv script
+  --Prog.Trace (show s)
+  runScrip env s
 
 conv :: Script -> Scrip
 conv = fromScript where
-
   fromScript :: Script -> Scrip
   fromScript = \case
     ShamError s -> QShamError s
@@ -419,21 +88,16 @@ conv = fromScript where
     ReadIntoVar x -> QReadIntoVar x
     Invoke1 step Wait -> fromStep step
     Invoke1 step NoWait -> QBackGrounding (fromStep step)
-
     Pipeline steps Wait -> QPipeline (map fromStep steps)
     Pipeline steps NoWait -> QBackGrounding (QPipeline (map fromStep steps))
 
   fromStep :: Step -> Scrip
   fromStep = \case
     XSubShell script rs -> QRedirecting (fromScript script) rs
-
     Run (Invocation (Word "exec") []) rs ->
       QExec (QRedirecting QNull rs)
-
     Run (Invocation (Word "exec") (w:ws)) rs ->
       QExec (QRedirecting (fromInvocation (Invocation w ws)) rs)
-
-
     Run invocation rs -> QRedirecting (fromInvocation invocation) rs
 
   fromInvocation :: Invocation -> Scrip
@@ -448,9 +112,7 @@ conv = fromScript where
     --Invocation (Word "exec") (w:ws) -> QExec (QInvoke w ws)
     Invocation w ws -> QInvoke w ws
 
-
-
-data Scrip -- no "t" for now
+data Scrip -- no "t" for now. TODO: regain "t" and loose Q prefix when conv step is avoided
   = QNull
   | QSeq Scrip Scrip
   | QIf Pred Scrip Scrip
@@ -466,7 +128,6 @@ data Scrip -- no "t" for now
   | QRedirecting Scrip [Redirect] -- TODO: have just 1 redirect!
   deriving Show
 
-
 -- done means we are in an Exec context and so can 'take-over' the process
 data K = Done | Cont (Env -> Prog ())
 
@@ -474,7 +135,6 @@ runK :: Env -> K -> Prog ()
 runK env = \case
   Done -> exit -- not pure () !!
   Cont k -> k env
-
 
 runScrip :: Env -> Scrip -> Prog ()
 runScrip env0 scrip0 = loop env0 scrip0 (Cont $ \_ -> pure ()) where
@@ -507,7 +167,7 @@ runScrip env0 scrip0 = loop env0 scrip0 (Cont $ \_ -> pure ()) where
 
     QEcho ws -> \k -> do
       args <- mapM (evalWord env) ws
-      runEcho args
+      builtinEcho args
       runK env k
 
     QExit -> \_ignored_k -> do
@@ -547,3 +207,107 @@ runScrip env0 scrip0 = loop env0 scrip0 (Cont $ \_ -> pure ()) where
       forkNoWait $ do
         loop env s Done
       runK env k
+
+
+evalPred :: Env -> Pred -> Prog Bool
+evalPred env = \case
+  Eq w1 w2 -> do
+    x1 <- evalWord env w1
+    x2 <- evalWord env w2
+    pure (x1 == x2)
+  NotEq w1 w2 -> do
+    x1 <- evalWord env w1
+    x2 <- evalWord env w2
+    pure (x1 /= x2)
+
+evalWord :: Env -> Word -> Prog String
+evalWord Env{pid,com,args,bindings} = \case
+  Word s -> pure s
+  DollarDollar -> let (Pid n) = pid in pure $ show n
+  DollarHash -> pure $ show (length args)
+  DollarN n ->
+    if n > length args
+    then do
+      write stderr ("$" ++ show n ++ " unbound")
+      pure ""
+    else pure $ (com:args)!!n
+  DollarName x ->
+    case Map.lookup x bindings of
+      Nothing -> do
+        write stderr ("$" ++ show x ++ " unbound")
+        pure ""
+      Just v ->
+        pure v
+
+builtinEcho :: [String] -> Prog ()
+builtinEcho args =
+  write stdout (unwords args)
+
+builtinRead :: Prog String
+builtinRead =
+  read NoPrompt stdin >>= \case
+    Left EOF -> exit
+    Right line -> return line
+
+loadShamScript :: Env -> String -> Prog Script
+loadShamScript env path = do
+  lines <- do
+    withOpen (Path.create path) OpenForReading $ \fd -> do
+      readAll fd
+  pure $ shamParser env lines
+
+execRedirect :: Env -> Redirect -> Prog ()
+execRedirect env = \case
+  Redirect om dest (FromPath path) -> do
+    path <- evalWord env path
+    withOpen (Path.create path) om $ \src -> do
+      dup2 dest src
+  Redirect _om dest (FromFD src) -> do -- do we care what the open-mode is?
+    dup2 dest src
+
+pipeline :: [Prog()] -> Prog ()
+pipeline = \case
+    [] -> error "runPipeline[]"
+    prog1:progs -> loop Nothing [] prog1 progs
+  where
+    loop :: Maybe FD -> [Pid] -> Prog () -> [Prog ()] -> Prog ()
+    loop incoming pids prog1 = \case
+      [] -> do
+        Prog.Fork >>= \case
+          Nothing -> do
+            case incoming of
+              Nothing -> pure ()
+              Just incoming -> do dup2 stdin incoming; Prog.Call Close incoming
+            prog1
+          Just childPid -> do
+            case incoming of
+              Nothing -> pure ()
+              Just incoming -> Prog.Call Close incoming
+            mapM_ Prog.Wait (childPid:pids)
+
+      prog2:progs -> do
+        PipeEnds{w=pipeW,r=pipeR} <- Prog.Call SysPipe ()
+        Prog.Fork >>= \case
+          Nothing -> do
+            case incoming of
+              Nothing -> pure ()
+              Just incoming -> do dup2 stdin incoming; Prog.Call Close incoming
+            Prog.Call Close pipeR
+            dup2 stdout pipeW; Prog.Call Close pipeW
+            prog1
+          Just childPid -> do
+            case incoming of
+              Nothing -> pure ()
+              Just incoming -> Prog.Call Close incoming
+            Prog.Call Close pipeW
+            loop (Just pipeR) (childPid:pids) prog2 progs
+
+dup2 :: FD -> FD -> Prog ()
+dup2 d s = do
+  Prog.Call Dup2 (d,s) >>= \case
+    Left BadFileDescriptor -> do
+      write stderr $ "bad file descriptor: " ++ show s
+      exit
+    Right () -> pure ()
+
+
