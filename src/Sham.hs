b8889e5506a6cf1f7da75dd3bd0b78f442e4cef1
Nick Chapman <nic@amadido.co.uk>
2021-03-27 09:59:47 +0000

    refactor parser/interpreter code split for sham

--- src/Sham.hs
+++ src/Sham.hs
@@ -1,19 +1,16 @@
 -- | 'sham' is a shell-style command interpreter which runs on MeNicks.
 module Sham (sham) where
 
-import EarleyM (Gram,fail,alts,getToken,many,skipWhile,ParseError(..),Ambiguity(..),SyntaxError(..))
+import Data.Map (Map)
 import Interaction (Prompt(..))
-import Lib (loadFile,read)
-import Misc (EOF(..))
-import Prelude hiding (Word,read,fail)
-import Prog (Prog,FD(..),Command(..),OpenMode(..),WriteOpenMode(..))
-import qualified Data.Char as Char
+import Lib (loadFile,stdin,stdout,stderr,read,write,exit,withOpen,readAll,execCommand,forkWait,forkNoWait)
+import Misc (EOF(..),PipeEnds(..))
+import Prelude hiding (Word,read)
+import Prog (Prog,FD(..),Command(..),OpenMode(..),SysCall(..),BadFileDescriptor(..),Pid(..))
+import Syntax (parseLine,Script(..),Word(..),Pred(..),Redirect(..),RedirectSource(..),Var(..))
 import qualified Data.Map.Strict as Map
-import qualified EarleyM as EM (parse,Parsing(..))
-import qualified Prog
-
-import qualified Script (Env(..), runScript)
-import Script (Script(..),Word(..),Pred(..),Redirect(..),RedirectSource(..),Var(..))
+import qualified Path (create)
+import qualified Prog (Prog(Argv,MyPid,Fork,Call,Wait))
 
 sham :: Prog ()
 sham = do
@@ -26,17 +23,17 @@ sham = do
       let com = "sham"
       let args = []
       let bindings = Map.empty
-      let env = Script.Env { pid, com, args, bindings, shamParser}
-      Script.runScript env script
+      let env = Env { pid, com, args, bindings, shamParser = parseLines}
+      runScript env script
 
     path:args -> do
       lines <- loadFile path
-      let script = shamParser lines
+      let script = parseLines lines
       pid <- Prog.MyPid
       let com = path
       let bindings = Map.empty
-      let env = Script.Env { pid, com, args, bindings, shamParser}
-      Script.runScript env script
+      let env = Env { pid, com, args, bindings, shamParser = parseLines}
+      runScript env script
 
     [] -> loop 1 where
       loop :: Int -> Prog ()
@@ -51,194 +48,203 @@ sham = do
             let com = "sham"
             let args = []
             let bindings = Map.empty
-            let env = Script.Env { pid, com, args, bindings, shamParser}
-            Script.runScript env script
+            let env = Env { pid, com, args, bindings, shamParser = parseLines}
+            runScript env script
             loop (n+1)
 
 
-shamParser :: [String] -> Script
-shamParser lines = foldl QSeq QNull (map parseLine lines)
-
-parseLine :: String -> Script
-parseLine str = do
-  case EM.parse (lang <$> getToken) str of
-    EM.Parsing{EM.outcome} -> case outcome of
-      Left pe -> QShamError $ prettyParseError str pe
-      Right script -> script
-
--- TODO: when parsing a script it would be nice to see the line number for a parse error
-prettyParseError :: String -> ParseError -> String
-prettyParseError str = \case
-  AmbiguityError (Ambiguity _ p1 p2) -> "ambiguous parse between positions " ++ show p1 ++ "--" ++ show p2
-  SyntaxError (UnexpectedTokenAt p) -> "unexpected '" ++ char p ++ "' at position " ++ show p
-  SyntaxError (UnexpectedEOF _) -> "unexpected end of line"
-  SyntaxError (ExpectedEOF p) -> "expected EOF at position " ++ show p
-  where char p = [str!!(p-1)]
-
-lang :: Gram Char -> Gram Script
-lang token = script0 where
-
-  script0 = do
-    ws
-    res <- alts [ do res <- script; ws; pure res
-                , do eps; pure QNull ]
-    alts [eps,lineComment]
-    pure res
-
-  lineComment = do symbol '#'; skipWhile (skip token)
-
-  script = alts [ pipeline, conditional, readIntoVar ]
-
-  conditional = do
-    keyword "if"
-    ws1; p <- pred
-    ws1; s <- step
-    pure $ QIf p s QNull
-
-  pred = alts [equal,notEqual]
-
-  equal = do
-    x1 <- word
-    ws; keyword "="; ws
-    x2 <- word
-    pure (Eq x1 x2)
-
-  notEqual = do
-    x1 <- word
-    ws; keyword "!="; ws
-    x2 <- word
-    pure (NotEq x1 x2)
-
-  readIntoVar = do
-    keyword "read"
-    ws1; x <- varname
-    pure $ QReadIntoVar x
-
-  pipeline = do
-    (x,xs) <- parseListSep step (do ws; symbol '|'; ws)
-    m <- mode
-    case xs of
-      [] -> pure (m x)
-      _ -> pure $ m $ QPipeline (x:xs)
-
-  mode :: Gram (Script -> Script) =
-    alts [ do eps; pure id,
-           do ws; symbol '&'; pure QBackGrounding ]
-
-  step = alts [run,exec,subshell]
-
-  subshell = do
-    symbol '('
-    script <- sequence
-    symbol ')'
-    rs <- redirects
-    pure $ (case rs of [] -> script; _-> QRedirecting script rs)
-
-  sequence = do
-    (x1,xs) <- parseListSep script (do ws; symbol ';'; ws)
-    pure $ foldl QSeq x1 xs
-
-  run = do
-    thing <- invocation
-    rs <- redirects
-    pure $ (case rs of [] -> thing; _ -> QRedirecting thing rs)
-
-  exec = do
-    keyword "exec"
-    ws1; thing <- invocation
-    rs <- redirects
-    pure $ QExec (case rs of [] -> thing; _ -> QRedirecting thing rs)
-
-  invocation = do
-    (com,args) <- parseListSep word ws1
-    case com of Word "read" -> fail; _ -> pure ()
-    case com of Word "exec" -> fail; _ -> pure ()
-    pure $ makeInvoke (com:args)
-
-  makeInvoke :: [Word] -> Script -- TODO: avoid this matching; just have alt parsers
-  makeInvoke = \case
-    [] -> undefined
-    (Word ".":w:ws) -> QSource w ws
-    [Word "."] -> QShamError "source takes at least one argument"
-    Word "echo":ws -> QEcho ws
-    [Word "exit"] -> QExit
-    (Word "exit":_) -> QShamError "exit takes no args"
-    w:ws -> QInvoke w ws
-
-  redirects = alts
-    -- TODO: Goal: allow just "ws" to separate args from redirects.
-    -- Problem is that currently this causes ambiguity for examples such as:
-    --  "echo foo1>xx"
-    -- It should parse as:       "echo foo1 >xx"
-    -- But we think it might be  "echo foo 1>xx"  !!
-    [ do ws1; (r1,rs) <- parseListSep redirect ws1; pure (r1:rs)
-    , do eps; pure []
-    ]
-
-  redirect = alts
-    [ do
-        dest <- alts [ do eps; pure 0, do n <- fd; pure n ]
-        let mode = OpenForReading
-        symbol '<'
-        ws
-        src <- redirectSource
-        pure $ Redirect mode dest src
-    , do
-        dest <- alts [ do eps; pure 1, do n <- fd; ws; pure n ]
-        mode <-
-          alts [ do symbol  '>';  pure $ OpenForWriting Truncate
-               , do keyword ">>"; pure $ OpenForWriting Append ]
-        ws
-        src <- redirectSource
-        pure $ Redirect mode dest src
-    ]
-
-  redirectSource = alts [ FromPath <$> word, FromFD <$> fdRef ]
-  fdRef = do symbol '&'; fd
-  fd = FD <$> digit -- TODO: multi-digit file-desciptors
-
-  word = alts [ Word <$> ident0
-              , do keyword "$$"; pure DollarDollar
-              , do keyword "$#"; pure DollarHash
-              , do keyword "$"; DollarN <$> digit
-              , do keyword "$"; DollarName <$> varname
-              ]
-
-  keyword string = mapM_ symbol string
-
-  varname = Var <$> do
-    x <- alpha
-    xs <- many (alts [alpha,numer])
-    pure (x : xs)
-
-  ident0 = do
-    x <- alts [alpha,numer,dash,dot,colon]
-    xs <- many (alts [alpha,numer,dash,dot,colon])
-    pure (x : xs)
-
-  digit = do c <- numer; pure (digitOfChar c)
-
-  alpha = sat Char.isAlpha
-  numer = sat Char.isDigit
-  dash = sat (== '-')
-  dot = sat (== '.')
-  colon = sat (== ':')
-  space = skip (sat Char.isSpace)
-
-  symbol x = do t <-token; if t==x then pure () else fail
-  sat pred = do c <- token; if pred c then pure c else fail
-
-  ws = skipWhile space -- white*
-  ws1 = do space; ws -- white+
-
-  skip p = do _ <- p; eps
-  eps = pure ()
+parseLines :: [String] -> Script
+parseLines lines = foldl QSeq QNull (map parseLine lines)
 
 
-digitOfChar :: Char -> Int
-digitOfChar c = Char.ord c - ord0 where ord0 = Char.ord '0'
+data Env = Env
+  { com :: String
+  , args :: [String]
+  , bindings :: Map Var String
+  , pid :: Pid
+  , shamParser :: [String] -> Script
+  }
 
-parseListSep :: Gram a -> Gram () -> Gram (a,[a])
-parseListSep p sep = alts [
-    do x <- p; sep; (x1,xs) <- parseListSep p sep; pure (x,x1:xs),
-    do x <- p; pure (x,[])]
+-- done means we are in an Exec context and so can 'take-over' the process
+data K = Done | Cont (Env -> Prog ())
+
+runK :: Env -> K -> Prog ()
+runK env = \case
+  Done -> exit -- not pure () !!
+  Cont k -> k env
+
+runScript :: Env -> Script -> Prog ()
+runScript env0 scrip0 = loop env0 scrip0 (Cont $ \_ -> pure ()) where
+
+  loop :: Env -> Script -> K -> Prog ()
+  loop env = \case
+    QNull -> \k -> runK env k
+
+    QSeq s1 s2 -> \k -> do
+      loop env s1 $ Cont $ \env -> loop env s2 k
+
+    QSource w ws -> \k -> do
+      com <- evalWord env w
+      args <- mapM (evalWord env) ws
+      script <- loadShamScript env com
+      runScript env { args } script
+      runK env k
+
+    QIf pred s1 s2 -> \k -> do
+      b <- evalPred env pred
+      loop env (if b then s1 else s2) k
+
+    QShamError mes -> \_ignored_k -> do
+      write stderr mes
+
+    QReadIntoVar x -> \k -> do
+      line <- builtinRead
+      let Env{bindings} = env
+      runK env { bindings = Map.insert x line bindings } k
+
+    QEcho ws -> \k -> do
+      args <- mapM (evalWord env) ws
+      builtinEcho args
+      runK env k
+
+    QExit -> \_ignored_k -> do
+      exit
+
+    QExec s -> \_ignored_k -> do
+      loop env s Done
+
+    QInvoke w ws -> \k -> do
+      com <- evalWord env w
+      args <- mapM (evalWord env) ws
+      case k of
+        Done -> execCommand (Command (com,args))
+        Cont k -> do
+          forkWait $
+            execCommand (Command (com,args))
+          k env
+
+    QRedirecting s [] -> \k -> loop env s k
+
+    QRedirecting s rs -> \k -> do
+      case k of
+        Done -> do
+          mapM_ (execRedirect env) rs
+          loop env s Done
+        Cont k -> do
+          forkWait $ do
+            mapM_ (execRedirect env) rs
+            loop env s Done
+          k env
+
+    QPipeline scrips -> \k -> do
+      pipeline (map (\s -> loop env s Done) scrips)
+      runK env k
+
+    QBackGrounding s -> \k -> do
+      forkNoWait $ do
+        loop env s Done
+      runK env k
+
+
+evalPred :: Env -> Pred -> Prog Bool
+evalPred env = \case
+  Eq w1 w2 -> do
+    x1 <- evalWord env w1
+    x2 <- evalWord env w2
+    pure (x1 == x2)
+  NotEq w1 w2 -> do
+    x1 <- evalWord env w1
+    x2 <- evalWord env w2
+    pure (x1 /= x2)
+
+evalWord :: Env -> Word -> Prog String
+evalWord Env{pid,com,args,bindings} = \case
+  Word s -> pure s
+  DollarDollar -> let (Pid n) = pid in pure $ show n
+  DollarHash -> pure $ show (length args)
+  DollarN n ->
+    if n > length args
+    then do
+      write stderr ("$" ++ show n ++ " unbound")
+      pure "" -- TODO: prefer to exit, but mustn't kill sham console
+    else pure $ (com:args)!!n
+  DollarName x ->
+    case Map.lookup x bindings of
+      Nothing -> do
+        write stderr ("$" ++ show x ++ " unbound")
+        pure ""
+      Just v ->
+        pure v
+
+builtinEcho :: [String] -> Prog ()
+builtinEcho args =
+  write stdout (unwords args)
+
+builtinRead :: Prog String
+builtinRead =
+  read NoPrompt stdin >>= \case
+    Left EOF -> exit
+    Right line -> return line
+
+loadShamScript :: Env -> String -> Prog Script
+loadShamScript env path = do
+  lines <- do
+    withOpen (Path.create path) OpenForReading $ \fd -> do
+      readAll fd
+  pure $ shamParser env lines
+
+execRedirect :: Env -> Redirect -> Prog ()
+execRedirect env = \case
+  Redirect om dest (FromPath path) -> do
+    path <- evalWord env path
+    withOpen (Path.create path) om $ \src -> do
+      dup2 dest src
+  Redirect _om dest (FromFD src) -> do -- do we care what the open-mode is?
+    dup2 dest src
+
+pipeline :: [Prog()] -> Prog ()
+pipeline = \case
+    [] -> error "runPipeline[]"
+    prog1:progs -> loop Nothing [] prog1 progs
+  where
+    loop :: Maybe FD -> [Pid] -> Prog () -> [Prog ()] -> Prog ()
+    loop incoming pids prog1 = \case
+      [] -> do
+        Prog.Fork >>= \case
+          Nothing -> do
+            case incoming of
+              Nothing -> pure ()
+              Just incoming -> do dup2 stdin incoming; Prog.Call Close incoming
+            prog1
+          Just childPid -> do
+            case incoming of
+              Nothing -> pure ()
+              Just incoming -> Prog.Call Close incoming
+            mapM_ Prog.Wait (childPid:pids)
+
+      prog2:progs -> do
+        PipeEnds{w=pipeW,r=pipeR} <- Prog.Call SysPipe ()
+        Prog.Fork >>= \case
+          Nothing -> do
+            case incoming of
+              Nothing -> pure ()
+              Just incoming -> do dup2 stdin incoming; Prog.Call Close incoming
+            Prog.Call Close pipeR
+            dup2 stdout pipeW; Prog.Call Close pipeW
+            prog1
+          Just childPid -> do
+            case incoming of
+              Nothing -> pure ()
+              Just incoming -> Prog.Call Close incoming
+            Prog.Call Close pipeW
+            loop (Just pipeR) (childPid:pids) prog2 progs
+
+dup2 :: FD -> FD -> Prog ()
+dup2 d s = do
+  Prog.Call Dup2 (d,s) >>= \case
+    Left BadFileDescriptor -> do
+      write stderr $ "bad file descriptor: " ++ show s
+      exit
+    Right () -> pure ()
+
+
---
+++ src/Syntax.hs
@@ -0,0 +1,236 @@
+-- | Parser and AST for 'sham' scripts
+module Syntax (
+  Script(..), Redirect(..), RedirectSource(..), Pred(..), Word(..), Var(..),
+  parseLine,
+  ) where
+
+import EarleyM (Gram,fail,alts,getToken,many,skipWhile,ParseError(..),Ambiguity(..),SyntaxError(..))
+import Prelude hiding (Word,read,fail)
+import Prog (FD(..),OpenMode(..),WriteOpenMode(..))
+import qualified Data.Char as Char
+import qualified EarleyM as EM (parse,Parsing(..))
+
+data Script -- TODO: loose Q prefix?
+  = QNull
+  | QSeq Script Script
+  | QIf Pred Script Script
+  | QShamError String
+  | QEcho [Word]
+  | QReadIntoVar Var
+  | QExit
+  | QExec Script
+  | QInvoke Word [Word]
+  | QSource Word [Word]
+  | QPipeline [Script]
+  | QBackGrounding Script
+  | QRedirecting Script [Redirect] -- TODO: have just 1 redirect!
+  deriving Show
+
+data Pred
+  = Eq Word Word
+  | NotEq Word Word
+  deriving Show
+
+data Word
+  = Word String
+  | DollarHash
+  | DollarN Int
+  | DollarDollar
+  | DollarName Var
+  deriving Show
+
+newtype Var = Var String
+  deriving (Eq,Ord)
+
+instance Show Var where show (Var s) = s
+
+data Redirect = Redirect OpenMode FD RedirectSource
+  deriving Show
+
+data RedirectSource = FromPath Word | FromFD FD
+  deriving Show
+
+
+parseLine :: String -> Script
+parseLine str = do
+  case EM.parse (lang <$> getToken) str of
+    EM.Parsing{EM.outcome} -> case outcome of
+      Left pe -> QShamError $ prettyParseError str pe
+      Right script -> script
+
+-- TODO: when parsing a script it would be nice to see the line number for a parse error
+prettyParseError :: String -> ParseError -> String
+prettyParseError str = \case
+  AmbiguityError (Ambiguity _ p1 p2) -> "ambiguous parse between positions " ++ show p1 ++ "--" ++ show p2
+  SyntaxError (UnexpectedTokenAt p) -> "unexpected '" ++ char p ++ "' at position " ++ show p
+  SyntaxError (UnexpectedEOF _) -> "unexpected end of line"
+  SyntaxError (ExpectedEOF p) -> "expected EOF at position " ++ show p
+  where char p = [str!!(p-1)]
+
+lang :: Gram Char -> Gram Script
+lang token = script0 where
+
+  script0 = do
+    ws
+    res <- alts [ do res <- script; ws; pure res
+                , do eps; pure QNull ]
+    alts [eps,lineComment]
+    pure res
+
+  lineComment = do symbol '#'; skipWhile (skip token)
+
+  script = alts [ pipeline, conditional, readIntoVar ]
+
+  conditional = do
+    keyword "if"
+    ws1; p <- pred
+    ws1; s <- step
+    pure $ QIf p s QNull
+
+  pred = alts [equal,notEqual]
+
+  equal = do
+    x1 <- word
+    ws; keyword "="; ws
+    x2 <- word
+    pure (Eq x1 x2)
+
+  notEqual = do
+    x1 <- word
+    ws; keyword "!="; ws
+    x2 <- word
+    pure (NotEq x1 x2)
+
+  readIntoVar = do
+    keyword "read"
+    ws1; x <- varname
+    pure $ QReadIntoVar x
+
+  pipeline = do
+    (x,xs) <- parseListSep step (do ws; symbol '|'; ws)
+    m <- mode
+    case xs of
+      [] -> pure (m x)
+      _ -> pure $ m $ QPipeline (x:xs)
+
+  mode :: Gram (Script -> Script) =
+    alts [ do eps; pure id,
+           do ws; symbol '&'; pure QBackGrounding ]
+
+  step = alts [run,exec,subshell]
+
+  subshell = do
+    symbol '('
+    script <- sequence
+    symbol ')'
+    rs <- redirects
+    pure $ (case rs of [] -> script; _-> QRedirecting script rs)
+
+  sequence = do
+    (x1,xs) <- parseListSep script (do ws; symbol ';'; ws)
+    pure $ foldl QSeq x1 xs
+
+  run = do
+    thing <- invocation
+    rs <- redirects
+    pure $ (case rs of [] -> thing; _ -> QRedirecting thing rs)
+
+  exec = do
+    keyword "exec"
+    ws1; thing <- invocation
+    rs <- redirects
+    pure $ QExec (case rs of [] -> thing; _ -> QRedirecting thing rs)
+
+  invocation = do
+    (com,args) <- parseListSep word ws1
+    case com of Word "read" -> fail; _ -> pure ()
+    case com of Word "exec" -> fail; _ -> pure ()
+    pure $ makeInvoke (com:args)
+
+  makeInvoke :: [Word] -> Script -- TODO: avoid this matching; just have alt parsers
+  makeInvoke = \case
+    [] -> undefined
+    (Word ".":w:ws) -> QSource w ws
+    [Word "."] -> QShamError "source takes at least one argument"
+    Word "echo":ws -> QEcho ws
+    [Word "exit"] -> QExit
+    (Word "exit":_) -> QShamError "exit takes no args"
+    w:ws -> QInvoke w ws
+
+  redirects = alts
+    -- TODO: Goal: allow just "ws" to separate args from redirects.
+    -- Problem is that currently this causes ambiguity for examples such as:
+    --  "echo foo1>xx"
+    -- It should parse as:       "echo foo1 >xx"
+    -- But we think it might be  "echo foo 1>xx"  !!
+    [ do ws1; (r1,rs) <- parseListSep redirect ws1; pure (r1:rs)
+    , do eps; pure []
+    ]
+
+  redirect = alts
+    [ do
+        dest <- alts [ do eps; pure 0, do n <- fd; pure n ]
+        let mode = OpenForReading
+        symbol '<'
+        ws
+        src <- redirectSource
+        pure $ Redirect mode dest src
+    , do
+        dest <- alts [ do eps; pure 1, do n <- fd; ws; pure n ]
+        mode <-
+          alts [ do symbol  '>';  pure $ OpenForWriting Truncate
+               , do keyword ">>"; pure $ OpenForWriting Append ]
+        ws
+        src <- redirectSource
+        pure $ Redirect mode dest src
+    ]
+
+  redirectSource = alts [ FromPath <$> word, FromFD <$> fdRef ]
+  fdRef = do symbol '&'; fd
+  fd = FD <$> digit -- TODO: multi-digit file-desciptors
+
+  word = alts [ Word <$> ident0
+              , do keyword "$$"; pure DollarDollar
+              , do keyword "$#"; pure DollarHash
+              , do keyword "$"; DollarN <$> digit
+              , do keyword "$"; DollarName <$> varname
+              ]
+
+  keyword string = mapM_ symbol string
+
+  varname = Var <$> do
+    x <- alpha
+    xs <- many (alts [alpha,numer])
+    pure (x : xs)
+
+  ident0 = do
+    x <- alts [alpha,numer,dash,dot,colon]
+    xs <- many (alts [alpha,numer,dash,dot,colon])
+    pure (x : xs)
+
+  digit = do c <- numer; pure (digitOfChar c)
+
+  alpha = sat Char.isAlpha
+  numer = sat Char.isDigit
+  dash = sat (== '-')
+  dot = sat (== '.')
+  colon = sat (== ':')
+  space = skip (sat Char.isSpace)
+
+  symbol x = do t <-token; if t==x then pure () else fail
+  sat pred = do c <- token; if pred c then pure c else fail
+
+  ws = skipWhile space -- white*
+  ws1 = do space; ws -- white+
+
+  skip p = do _ <- p; eps
+  eps = pure ()
+
+
+digitOfChar :: Char -> Int
+digitOfChar c = Char.ord c - ord0 where ord0 = Char.ord '0'
+
+parseListSep :: Gram a -> Gram () -> Gram (a,[a])
+parseListSep p sep = alts [
+    do x <- p; sep; (x1,xs) <- parseListSep p sep; pure (x,x1:xs),
+    do x <- p; pure (x,[])]
