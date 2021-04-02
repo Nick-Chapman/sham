7fd381e845895d0965a58e50c95237493264349b
Nick Chapman <nic@amadido.co.uk>
2021-04-02 13:43:15 +0100

    remove monitor code from Init.init

--- src/Init.hs
+++ src/Init.hs
@@ -1,41 +1,14 @@
 -- | 'init' is the top-level program which runs on MeNicks
 module Init (init) where
 
-import Interaction (Prompt(..),OutMode(..),EOF(..))
-import Lib (tryLoadBinary,forkWait,write,stdout,stderr,exit,execSameEnv,shift2,closeAllBut,close,read)
+import Lib (forkWait,tryLoadBinary,write,stderr,exit,execSameEnv)
 import Prelude hiding (init,read)
-import Prog (Prog(..),FD,SysCall(..),Command(..),PipeEnds(..))
+import Prog (Prog(..),Command(..))
 
 init :: Prog ()
 init = do
-  forkWait $ do -- not essential; allows "init" process to remain
-    let _ = monitor (100+stderr) StdErr -- TODO: step to removing. put on fd 102
-    let _ = monitor (100+stdout) StdOut -- TODO: step to removing. put on fd 101
+  forkWait $ do
     tryLoadBinary "sham" >>= \case
       Nothing -> do write stderr "init : cannot find sham interpreter"; exit
       Just prog -> do
         execSameEnv (Command ("sham",[])) prog
-
-monitor :: FD -> OutMode -> Prog ()
-monitor dest mode = do
-    PipeEnds{r,w} <- Call SysPipe ()
-    Prog.Fork >>= \case
-      Nothing -> do
-        -- continue in the child!
-        close r
-        shift2 dest w
-      Just pid -> do
-        closeAllBut [r]
-        let
-          loop :: Prog ()
-          loop = do
-            Alive pid >>= \case
-              False -> pure ()
-              True -> do
-                read NoPrompt r >>= \case
-                  Left EOF -> pure ()
-                  Right line -> do
-                    WriteConsole mode line
-                    loop
-        let com = "tty-"++show mode
-        execSameEnv (Command (com,[])) loop
